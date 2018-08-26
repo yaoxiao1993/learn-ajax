12736b8e528901ea769c35a6030b094f4297c281
yaoxiaoyu <yaoxiao0525@foxmail.com>
2018-08-26 22:26:20 +0800

    finished add movie first step

---
+++ client/js/addMovie.js
@@ -0,0 +1,31 @@
+window.onload=function(){
+    let addButton = document.getElementById("addButton");
+    const form = this.document.getElementById("addMovie");
+    addButton.addEventListener('click',e=>{
+        e.preventDefault();
+        const movieData = getMovieData("addMovie");
+        console.log(movieData)
+        axios.get('http://localhost:3000/movies')
+            .then(function(response){
+                if(response.data){
+                    alert("添加成功！");
+                    form.reset();
+                }
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+    },false);
+}
+
+function getMovieData(formId){
+    const form = document.getElementById(formId);
+    let tagElements = form.getElementsByTagName('input');
+    let movieData = {};
+    for(let item of tagElements){
+        movieData[item.name] = item.value;
+    }
+    return movieData;
+}
+
+e.preventDefault();
